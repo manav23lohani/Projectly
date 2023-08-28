f91ff8667c0b001fced6c183d5fd63eb79c410d4
manav23lohani <manav23lohani@gmail.com>
2023-08-28 11:37:42 +0530

    cyclic.sh

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const {notFound,errorHandler} = require("./middlewares/errorHandler");
 const DBconnect = require("./utils/dbConnection");
-require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const PORT = process.env.PORT || 5001;
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 app.use(express.json());
-DBconnect();
-
 app.use(cors());
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
@@ -16,6 +17,8 @@ app.use("/api/projectMember", require("./routes/requestRoutes"));
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(5001, ()=>{
-    console.log("Server is running");
-});
+DBconnect().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const DBconnect = ()=>mongoose.connect(process.env.DB_LINK)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+const DBconnect = async() => {
+  try {
+    await mongoose.connect(process.env.DB_LINK);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 module.exports = DBconnect;
